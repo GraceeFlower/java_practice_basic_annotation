dfbf6847f88357fe1c57dcf215aa3e439053084c
GraceeFlower <1491644103@qq.com>
2020-02-27 22:45:35 +0800

    [WM] feat: add the Alias annotation

--- annotation/src/main/java/com/thoughtworks/model/Student.java
+++ annotation/src/main/java/com/thoughtworks/model/Student.java
@@ -1,9 +1,25 @@
 package com.thoughtworks.model;
 
 // 为Student类添加一个Alias注解, 表明Student类可以取一个自定义的别名
+@Alias("Grace")
 public class Student {
 
     // 为age字段添加一个Limit注解, 表明age字段的最小值为1 最大值为140
     private int age;
+
+    public Student() {
+    }
+
+    public Student(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }
